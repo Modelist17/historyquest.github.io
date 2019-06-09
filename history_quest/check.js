691b3c7b2fabd437c5e17dfa8d3a01c268529084
Modelist17 <51048756+Modelist17@users.noreply.github.com>
2019-06-09 14:59:29 +0300

    Add files via upload

--- history_quest/check.js
+++ history_quest/check.js
@@ -15,24 +15,31 @@ var score = 0;
 var data;
 
 $('document').ready(function () {
-    console.log("Document Ready");
+  console.log("Document Ready");
 
-    $('#game_form').submit(function (event) {
-        score = 0;
-        
-        for (var i = 1; i <= 10; i++) {
-            if ($('#a'+i).val().toString() == A[i-1]) {
-                score++;
-            }
-        }
-        
-        event.preventDefault();
-        var data = $(this).serialize();
-        console.log(data);
-        
-        console.log(score);
-        
-        window.location.href="end_game_1.html";
-    });
+  $('#game_form').submit(function (event) {
+    score = 0;
+
+    for (var i = 1; i <= 10; i++) {
+      if ($('#a' + i).val().toString() == A[i - 1]) {
+        score++;
+      }
+    }
+
+    event.preventDefault();
+    var data = $(this).serialize();
+    console.log(data);
+
+    console.log(score);
+
+    if (score >= 9) {
+      window.location.href = "end_game_1.html";
+    } else if (score >= 4 && score <= 8) {
+      window.location.href = "end_game_2.html";
+    } else {
+      window.location.href = "end_game_3.html";
+
+    }
+  });
 
 });
